df5472edabe516bc3f9b65d79483d05a919c1ee0
Aryan Sangwan <55026638+ATV3320@users.noreply.github.com>
2023-02-22 23:46:54 +0530

    needed

--- codebase/src/HelloWorld.sol
+++ codebase/src/HelloWorld.sol
@@ -1,12 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "ds-test/test.sol";
+
+interface CheatCodes {
+   // Gets address for a given private key, (privateKey) => (address)
+   function addr(uint256) external returns (address);
+}
+
 contract HelloWorld {
   string private greeting;
   uint public version = 0;
   
+  address public addr1;
+  CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
+  
   constructor (string memory _greeting) {
     greeting = _greeting;
+    addr1 = cheats.addr(1);
   }
 
   function greet() public view returns(string memory) {
@@ -16,5 +27,6 @@ contract HelloWorld {
   function updateGreeting(string memory _greeting) public {
     version += 1;
     greeting = _greeting;
+
   }
 }
---
+++ codebase/src/contract1.sol
@@ -0,0 +1,8 @@
+// //SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.13;
+
+// contract StakingContract {
+//     function stake(uint amount, address token) external returns(bool) {
+
+//     }
+// }
